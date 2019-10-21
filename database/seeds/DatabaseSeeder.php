6e9b428ee09cfc9dc555326a52ab0c8238121473
yourmaze <asu@minus-30.ru>
2019-10-21 11:19:08 +0700

    Initial commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+         $this->call(UsersTableSeeder::class);
+         $this->call(SettingsTableSeeder::class);
+
+        $this->call(DoctorTableSeeder::class);
+
+        $this->call(InpatientTableSeeder::class);
+
+        $this->call(PatientTableSeeder::class);
+
+        $this->call(AppointmentTableSeeder::class);
+
+        $this->call(StaffTableSeeder::class);
+
+
+    }
+}
